df6e7c083c14ec02cfabc1e9d03fef0b04d9a3f7
DESKTOP-5SNT74G\torres <diogo.torres.30@gmail.com>
2017-10-10 00:39:08 +0100

    if windows/apple

--- exercise3/MicroMachines.h
+++ exercise3/MicroMachines.h
@@ -5,7 +5,11 @@
 
 #include <math.h>
 #include <GL/glew.h>
+#ifdef __APPLE__
 #include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
 
 #include "OrthogonalCamera.h"
 #include "PerspectiveCamera.h"
