dd6343be6c16a0aaf6ad0c0306d240ab31798968
nekrotzar <peixeocavalovoador@hotmail.com>
2017-10-11 19:55:08 +0100

    Exercise 2 and 3 folders deleted

--- exercise3/Camera.cpp
+++
@@ -1,7 +0,0 @@
-#include "Camera.h"
-
-Camera::Camera(){}
-Camera::~Camera(){}
-
-void Camera::setAt(float x, float y, float z){}
-void Camera::update(float ratio){}
--- exercise3/StaticObject.h
+++
@@ -1,8 +0,0 @@
-#ifndef StaticObject_h
-#define StaticObject_h
-
-#include "Object.h"
-
-class StaticObject : public Object{};
-
-#endif /* StaticObject_h */
