60559848a5329cc013f03a83156c72b094748c84
catarinapessoajorge <chatarina_jorge@hotmail.com>
2017-11-19 18:46:17 +0000

    s

--- MicroMachines.cpp
+++ MicroMachines.cpp
@@ -428,23 +428,7 @@ int MicroMachines::collides() {
             return i;
         }
     }
-    for (int i = 13; i < 94; i++) {#ifndef LensFlare_h
-#define LensFlare_h
-        
-#include <stdio.h>
-#include "Object.h"
-        
-        
-        class LensFlare : public Object {
-        protected:
-            float relativePositions[4] = {0.7f, 0.8f, 0.9f, 1.0f};
-        public:
-            LensFlare();
-            void render(VSShaderLib shader, GLint pvm_uniformId, GLint vm_uniformId, GLint normal_uniformdId, GLint texMode_uniformId);
-            void render_flare(VSShaderLib shader, int lx, int ly, int cx, int cy, GLint pvm_uniformId, GLint vm_uniformId, GLint normal_uniformdId, GLint texMode_uniformId);
-        };
-        
-#endif /* LensFlare_h */
+    for (int i = 13; i < 94; i++) {
         if ((_objects[i]->getPosition().getX() - 0.4 < _car->getPosition().getX()) &&
             (_car->getPosition().getX() < _objects[i]->getPosition().getX() + 0.4) &&
             (_objects[i]->getPosition().getZ() - 0.4 < _car->getPosition().getZ()) &&
